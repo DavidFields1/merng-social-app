a0d6ba3cb63f1ca4361ad561e19448dfffab47b9
David <david.campos.solorio@gmail.com>
2021-07-29 19:45:50 -0500

    likes to posts & like and comment count in posts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,19 @@
 const postsResolvers = require('./posts');
 const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
 
 module.exports = {
+    Post: {
+        // every query or mutation pass through this
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
     Mutation: {
-        ...usersResolvers.Mutation
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
     }
 }
